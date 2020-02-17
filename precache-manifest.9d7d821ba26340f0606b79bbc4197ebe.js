785c0eb2906eff5269297c938ac6f30529344653
Jonathan Champion <jonathan.champion@nakedwines.com>
2020-02-17 20:02:12 +0000

    Updates

---
+++ precache-manifest.9d7d821ba26340f0606b79bbc4197ebe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6bd9f7c0fd4d5f8ca84cb12cdd6557",
+    "url": "/naked-wines-tech-challenge/index.html"
+  },
+  {
+    "revision": "e9e38271d0a364da587c",
+    "url": "/naked-wines-tech-challenge/static/css/main.76eef140.chunk.css"
+  },
+  {
+    "revision": "93b059c82025cb7f81a2",
+    "url": "/naked-wines-tech-challenge/static/js/2.71df8263.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/naked-wines-tech-challenge/static/js/2.71df8263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e38271d0a364da587c",
+    "url": "/naked-wines-tech-challenge/static/js/main.3e99bfd4.chunk.js"
+  },
+  {
+    "revision": "2b3705e888a510a00a44",
+    "url": "/naked-wines-tech-challenge/static/js/runtime-main.5c3819ba.js"
+  },
+  {
+    "revision": "8d85c2f93f1e83a3faccc81b9954d857",
+    "url": "/naked-wines-tech-challenge/static/media/red-wine-placeholder.8d85c2f9.jpg"
+  },
+  {
+    "revision": "4d07052bca8c9bc54f4624ff1906a911",
+    "url": "/naked-wines-tech-challenge/static/media/rose-wine-placeholder-2.4d07052b.jpg"
+  },
+  {
+    "revision": "db8c67bb2b4c7860659ffdaa0609df37",
+    "url": "/naked-wines-tech-challenge/static/media/sparkling-wine-placeholder-2.db8c67bb.jpeg"
+  },
+  {
+    "revision": "88493f7c17952fbdfc7b75012f3d79a6",
+    "url": "/naked-wines-tech-challenge/static/media/white-wine-placeholder.88493f7c.jpg"
+  }
+]);
